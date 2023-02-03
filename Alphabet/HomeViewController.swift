c0f02b94f5f7a156fe0fd5a3a89eb09908838694
Maxim Papier <maxim-paper@yandex.ru>
2023-02-03 10:44:52 +0600

    + Setting up the UICollectionView()
    [bug] — the collection doesn't show in the simulator.

---
+++ Alphabet/HomeViewController.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    private let reuseIdentifier = "Cell"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        view.addSubview(collectionView)
+        setConstraints()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+}
+
+
+// MARK: - Data source
+
+extension HomeViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+    
+    
+    
+}
