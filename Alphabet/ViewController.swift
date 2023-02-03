184c13fdddb35fcbc54e7ce031b4eb453eaefaeb
Maxim Papier <maxim-paper@yandex.ru>
2023-02-03 06:39:18 +0600

    Initial Commit

---
+++ Alphabet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alphabet
+//
+//  Created by Maxim V. Brykov on 03.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
